df6762f20da0f50580bec1221cb591d9f33a93ef
Julian <ambient.solace@yahoo.com>
2016-02-24 01:04:49 +0000

    styled forms

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,9 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
-  get 'status_pages/abou'
-
-  get 'status_pages/contact'
-
   get 'static_pages/index'
+
+  post 'static_pages/thank_you'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
