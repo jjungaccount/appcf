6b216d0567d1c4d0a29c1e3ed26375a1c4ef0623
Julian <ambient.solace@yahoo.com>
2016-02-24 20:35:29 +0000

    configured emailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,4 +6,15 @@ class StaticPagesController < ApplicationController
     @products = Product.limit(3)
     @featured_product = Product.first
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+      :to => 'ambient.solace@yahoo.com',
+      :subject => "A new contact form message from #{@name}",
+      :body =>  @message).deliver
+  end
+
 end
