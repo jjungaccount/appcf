b0a845b73c9c708813c0590a332bde78bd32680d
Julian <ambient.solace@yahoo.com>
2016-03-03 23:23:44 +0000

    added authorization to Products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,8 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-
+  before_filter :authenticate_user!
+  load_and_authorize_resource
+  
   # GET /products
   # GET /products.json
   def index
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :orders
+
+  def admin?
+    admin
+  end
+  
 end
