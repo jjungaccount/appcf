4ae74a3d370931406c56e45e07561b35c0ba3172
Julian <ambient.solace@yahoo.com>
2016-03-03 23:43:34 +0000

    added authorization to Products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:index, :show]
   load_and_authorize_resource
   
   # GET /products
