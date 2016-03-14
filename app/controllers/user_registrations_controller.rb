06c85595bda0e6f215b999a8216c0b23f43d4388
Julian <ambient.solace@yahoo.com>
2016-03-14 23:53:25 +0000

    added Rspec

--- app/controllers/user_registrations_controller.rb
+++ app/controllers/user_registrations_controller.rb
@@ -2,7 +2,7 @@ class UserRegistrationsController < Devise::RegistrationsController
   def create
     super
     if @user.persisted?
-      UserMailer.welcome(@user).deliver
+      UserMailer.welcome(@user).deliver_now
     end
   end
 end
