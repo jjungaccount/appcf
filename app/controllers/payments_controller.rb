36264a2156ccacfe9189e6632c075ee57f6a24f7
Julian <ambient.solace@yahoo.com>
2016-03-31 00:54:19 +0000

    set price to decimal

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -6,7 +6,7 @@ class PaymentsController < ApplicationController
     # Create the charge on Stripe's servers - this will charge the user's card
     begin
       charge = Stripe::Charge.create(
-        :amount => @product.price, # amount in cents, again
+        :amount => @product.price_in_cents, # amount in cents, again
         :currency => "usd",
         :source => token,
         :description => params[:stripeEmail]
@@ -21,13 +21,13 @@ class PaymentsController < ApplicationController
       # The card has been declined
       body = e.json_body
       err = body[:error]
-      flash[:error] = "Unfortunately there was an error processing your payment: #{err[:message]}"
+      flash[:alert] = "Unfortunately there was an error processing your payment: #{err[:message]}"
       redirect_to product_path(@product)
     rescue Stripe::InvalidRequestError => e
       # Invalid parameters were supplied to Stripe's API
       body = e.json_body
       err = body[:error]
-      flash[:error] = "Unfortunately there was an error processing your payment: #{err[:message]}"
+      flash[:alert] = "Unfortunately there was an error processing your payment: #{err[:message]}"
       redirect_to product_path(@product)
     end
   end
