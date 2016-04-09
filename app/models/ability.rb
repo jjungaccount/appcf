4626e574204f8b11878f1e76d9886ff5c09ad6fb
Julian <ambient.solace@yahoo.com>
2016-04-09 01:49:31 +0000

    removed chosen gem

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,7 +7,7 @@ class Ability
       can :manage, :all
     else
       can :manage, User, id: user.id 
-      can :read, Product
+      can :read, Product, sold_date: nil
       can [:read, :create], Comment
     end     
   end
